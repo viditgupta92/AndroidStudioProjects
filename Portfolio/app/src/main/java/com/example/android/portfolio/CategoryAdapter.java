7c8a69d52bbb713ff0d1012f66b37b1850ba12be
viditgupta <gupta.vid@husky.neu.edu>
2017-10-12 21:06:07 -0700

    Portfolio app - Fragments commit

---
+++ Portfolio/app/src/main/java/com/example/android/portfolio/CategoryAdapter.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.portfolio;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Provides the appropriate {@link Fragment} for a view pager.
+ */
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    final int PAGE_COUNT = 6;
+
+    private Context mContext;
+
+    private String tabTitles[] = new String[] { "About", "Education", "Skills", "WorkExp", "Projects", "Achievements" };
+
+    public CategoryAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new AboutFragment();
+        }
+        else if (position == 1){
+            return new EducationFragment();
+        } else if (position == 2) {
+            return new SkillsFragment();
+        } else if (position == 3) {
+            return new WorkExpFragment();
+        } else if (position == 4) {
+            return new ProjectsFragment();
+        } else {
+            return new AchievementsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        if (position == 0) {
+            return mContext.getString(R.string.category_about);
+        } else if (position == 1){
+            return mContext.getString(R.string.category_education);
+        } else if (position == 2) {
+            return mContext.getString(R.string.category_skills);
+        } else if (position == 3) {
+            return mContext.getString(R.string.category_workexp);
+        }else if (position == 4) {
+            return mContext.getString(R.string.category_projects);
+        }else {
+            return mContext.getString(R.string.category_achievements);
+        }
+    }
+}
---
+++ Portfolio/app/src/main/java/com/example/android/portfolio/SkillsFragment.java
@@ -0,0 +1,19 @@
+package com.example.android.portfolio;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SkillsFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_skills, container, false);
+    }
+}

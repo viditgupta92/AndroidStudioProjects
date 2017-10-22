6913f886691736d68436f2272336acd034e0d2b5
viditgupta <gupta.vid@husky.neu.edu>
2017-10-21 19:16:20 -0700

    updated tabs and about section

--- Portfolio/app/src/main/java/com/example/android/portfolio/AboutFragment.java
+++ Portfolio/app/src/main/java/com/example/android/portfolio/AboutFragment.java
@@ -1,11 +1,14 @@
 package com.example.android.portfolio;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +20,94 @@ public class AboutFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
+        ImageView linkedin = (ImageView) rootView.findViewById(R.id.linkedin);
+        ImageView facebook = (ImageView) rootView.findViewById(R.id.facebook);
+        ImageView github = (ImageView) rootView.findViewById(R.id.github);
+        ImageView website = (ImageView) rootView.findViewById(R.id.website);
+
+        linkedin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get url from tag
+                String url = (String)view.getTag();
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+                //pass the url to intent data
+                intent.setData(Uri.parse(url));
+
+                startActivity(intent);
+            }
+        });
+
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get url from tag
+                String url = (String)view.getTag();
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+                //pass the url to intent data
+                intent.setData(Uri.parse(url));
+
+                startActivity(intent);
+            }
+        });
+
+        github.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get url from tag
+                String url = (String)view.getTag();
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+                //pass the url to intent data
+                intent.setData(Uri.parse(url));
+
+                startActivity(intent);
+            }
+        });
+
+        website.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get url from tag
+                String url = (String)view.getTag();
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+                //pass the url to intent data
+                intent.setData(Uri.parse(url));
+
+                startActivity(intent);
+            }
+        });
+
         return rootView;
+    }
+
+    public void openBrowser(View view){
+
+        //Get url from tag
+        String url = (String)view.getTag();
+
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+        //pass the url to intent data
+        intent.setData(Uri.parse(url));
+
+        startActivity(intent);
     }
 }
--- Portfolio/app/src/main/java/com/example/android/portfolio/MainActivity.java
+++ Portfolio/app/src/main/java/com/example/android/portfolio/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.portfolio;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 
@@ -21,5 +22,9 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }

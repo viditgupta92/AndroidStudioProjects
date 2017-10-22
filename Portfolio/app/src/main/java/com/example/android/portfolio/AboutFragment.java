024348aa51009bf307a50519f3ae57321df606cf
viditgupta <gupta.vid@husky.neu.edu>
2017-10-22 15:33:05 -0700

    modified about fragment to add resume and pronounciation

--- Portfolio/app/src/main/java/com/example/android/portfolio/AboutFragment.java
+++ Portfolio/app/src/main/java/com/example/android/portfolio/AboutFragment.java
@@ -1,29 +1,126 @@
 package com.example.android.portfolio;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class AboutFragment extends Fragment {
+
+    private MediaPlayer mp;
+
+    private AudioManager am;
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+    private AudioManager.OnAudioFocusChangeListener  mFocusChangeListener = new AudioManager.OnAudioFocusChangeListener(){
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if(focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                    focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK){
+                mp.pause();
+                mp.seekTo(0);
+            }
+            else if(focusChange == AudioManager.AUDIOFOCUS_GAIN){
+                mp.start();
+            }
+            else if(focusChange == AudioManager.AUDIOFOCUS_LOSS){
+                mp.stop();
+                releaseMediaPlayer();
+            }
+        }
+    };
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mp != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mp.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mp = null;
+
+            am.abandonAudioFocus(mFocusChangeListener);
+        }
+    }
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        super.onCreate(savedInstanceState);
+
+        am = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
+
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
+        ImageView pronounce = (ImageView) rootView.findViewById(R.id.pronounce_name);
+        Button resume = (Button) rootView.findViewById(R.id.resume);
         ImageView linkedin = (ImageView) rootView.findViewById(R.id.linkedin);
         ImageView facebook = (ImageView) rootView.findViewById(R.id.facebook);
         ImageView github = (ImageView) rootView.findViewById(R.id.github);
         ImageView website = (ImageView) rootView.findViewById(R.id.website);
+
+        pronounce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                releaseMediaPlayer();
+
+                int result = am.requestAudioFocus(mFocusChangeListener, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+
+                if(result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+
+                    mp = MediaPlayer.create(getActivity(), R.raw.pronounce);
+
+                    mp.start();
+
+                    mp.setOnCompletionListener(mCompletionListener);
+                }
+
+            }
+        });
+
+        resume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get url from tag
+                String url = (String)view.getTag();
+
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+
+                //pass the url to intent data
+                intent.setData(Uri.parse(url));
+
+                startActivity(intent);
+            }
+        });
 
         linkedin.setOnClickListener(new View.OnClickListener() {
             @Override
